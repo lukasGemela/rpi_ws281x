dd313b2991ed1bc1a3a4a310e9cb1ac92ee6bdfe
Lukas Gemela <lukas.gemela@gmail.com>
2020-01-04 16:31:37 +0100

    uy68ui6

--- mailbox.c
+++ mailbox.c
@@ -278,6 +278,7 @@ int mbox_open(void) {
     }
     file_desc = open(filename, 0);
     if (file_desc < 0) {
+        perror(errno);
         perror("Can't open device file\n");
         unlink(filename);
         return -1;
